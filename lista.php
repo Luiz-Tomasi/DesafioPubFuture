9a7eda86279f82d0ffebce34106984cef7be96ca
Neto <netolt123@hotmail.com>
2022-01-09 07:10:58 -0300

    Criado um lista.php

---
+++ lista.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!–- Links font-awesome e css -–>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <title>Lista</title>
+</head>
+<body>
+<!–- Navigation -–>
+    <div class="container">
+        <div class="navigation">
+            <ul>
+                <li>
+                    <a href="">
+                        <span class="fas fa-chart-pie"></span>
+                        <span class="title"> DesafioPubFuture</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php">
+                        <span class="fas fa-chart-pie"></span>
+                        <span class="title"> Cadastro</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span class="fas fa-chart-pie"></span>
+                        <span class="title"> Lista</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span class="fas fa-chart-pie"></span>
+                        <span class="title"> Dashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span class="fas fa-chart-pie"></span>
+                        <span class="title"> Dashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span class="fas fa-chart-pie"></span>
+                        <span class="title"> Dashboard</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+<!–- Navigation -–>
+</body>
+</html>
