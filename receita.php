3610a3d6cfa591ebe5f6a1b3baebb528f60c164b
Neto <netolt123@hotmail.com>
2022-01-08 21:06:53 -0300

    Feito uma função para inserir o cadastro no banco de dados

--- receita.php
+++ receita.php
@@ -1,5 +1,14 @@
 <?php
-
-
-
+    function insert(){
+        include('conexao.php');
+        include_once('index.php');
+        $valorR = $_POST['valorR'];
+        $dateRR = $_POST['dateRR'];
+        $dateRE = $_POST['dateRER'];
+        $contaR = $_POST['contaR'];
+        $tipoR = $_POST['tipoR'];
+        $descricaoR = $_POST['descricaoR'];
+        mysqli_query($mysqli, "INSERT INTO receita(valor, dataRecebimento , dataRecebimentoEsperado, descrição, tipoReceita, conta)
+        VALUES ('$valorR', '$dateRR', '$dateRE', '$descricaoR', '$tipoR', '$contaR')");
+    }
 ?>
